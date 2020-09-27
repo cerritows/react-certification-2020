0261cca786fc3b27955557838463f2af74ad01a0
Angeles Ramirez Cerritos <maria.ramirez@wizeline.com>
2020-09-27 03:44:08 -0500

    Add layout to search bar

--- src/components/Search/SearchBar.component.jsx
+++ src/components/Search/SearchBar.component.jsx
@@ -4,32 +4,36 @@ import { FilledButton } from '../Button';
 
 import { TextInput } from '../Input';
 import { Icon } from '../Icon';
-import Container from '../Layout';
+import Container, { Part } from '../Layout';
 
 function SearchBar({ setSearchTerms }) {
   const [searchTerms, setLocalSearchTerms] = useState('');
 
   return (
     <Container direction="row" align="center">
-      <TextInput
-        value={searchTerms}
-        onChange={(e) => {
-          console.log('Local search', e.target.value);
-          setLocalSearchTerms(e.target.value);
-        }}
-      >
-        <Icon name="Search" size="2rem" />
-      </TextInput>
-      <FilledButton
-        type="submit"
-        onClick={() => {
-          console.log('Search terms', searchTerms);
-          setSearchTerms(searchTerms);
-        }}
-        outlined
-      >
-        SEARCH
-      </FilledButton>
+      <Part bg="6" md="10">
+        <TextInput
+          value={searchTerms}
+          onChange={(e) => {
+            console.log('Local search', e.target.value);
+            setLocalSearchTerms(e.target.value);
+          }}
+        >
+          <Icon name="Search" size="2rem" />
+        </TextInput>
+      </Part>
+      <Part bg="1" md="10">
+        <FilledButton
+          type="submit"
+          onClick={() => {
+            console.log('Search terms', searchTerms);
+            setSearchTerms(searchTerms);
+          }}
+          outlined
+        >
+          SEARCH
+        </FilledButton>
+      </Part>
     </Container>
   );
 }
