d7891a63c61745279932b89ea64e8f8b72d4ad24
Angeles Ramirez Cerritos <maria.ramirez@wizeline.com>
2020-09-27 03:46:25 -0500

    Add login page changes

--- src/pages/Login/Login.page.jsx
+++ src/pages/Login/Login.page.jsx
@@ -1,38 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useHistory } from 'react-router';
 
 import { useAuth } from '../../providers/Auth';
-import './Login.styles.css';
+
+import Container, { Part } from '../../components/Layout';
+import { InputGroup, InputLabel, TextInput } from '../../components/Input';
+import { FilledButton } from '../../components/Button';
 
 function LoginPage() {
   const { login } = useAuth();
   const history = useHistory();
 
+  console.log('Login');
+
   function authenticate(event) {
     event.preventDefault();
     login();
-    history.push('/secret');
+    history.push('/');
   }
 
   return (
-    <section className="login">
-      <h1>Welcome back!</h1>
-      <form onSubmit={authenticate} className="login-form">
-        <div className="form-group">
-          <label htmlFor="username">
-            <strong>username </strong>
-            <input required type="text" id="username" />
-          </label>
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">
-            <strong>password </strong>
-            <input required type="password" id="password" />
-          </label>
-        </div>
-        <button type="submit">login</button>
-      </form>
-    </section>
+    <Container align="center">
+      <Part align="center" bg="8" md="10">
+        <h1>Welcome back!</h1>
+      </Part>
+
+      <Part bg="6" md="10">
+        <form onSubmit={authenticate} className="login-form">
+          <InputGroup>
+            <InputLabel htmlFor="username">
+              <strong>User</strong>
+              <TextInput required type="text" id="username" />
+            </InputLabel>
+          </InputGroup>
+
+          <InputGroup>
+            <InputLabel htmlFor="password">
+              <strong>Password</strong>
+              <TextInput required type="password" id="password" />
+            </InputLabel>
+          </InputGroup>
+          <FilledButton type="submit">Login</FilledButton>
+        </form>
+      </Part>
+    </Container>
   );
 }
 
