6fa6e87ffd8b8e096fbd0d50d81544ea9457890f
Angeles Ramirez Cerritos <maria.ramirez@wizeline.com>
2020-09-27 03:48:01 -0500

    Add App component new layout

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,80 +1,40 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import AuthProvider from '../../providers/Auth';
+import FavoritesProvider from '../../providers/Favorites';
+
 import HomePage from '../../pages/Home';
-import NavBar from '../NavBar/NavBar.component';
-import NavBarItem from '../NavBar/NavBarItem.component';
 
-import VideoPage from '../../pages/Video/Video.page';
+import VideoPage from '../../pages/Video/VideoDetail.page';
+import LoginPage from '../../pages/Login';
+import FavoritesPage from '../../pages/Favorites';
+import NotFound from '../../pages/NotFound';
+
+import GlobalStyle from '../Style';
+
+import TopBar from '../TopBar';
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar>
-        <NavBarItem>
-          <Link to="/login">Log in</Link>
-        </NavBarItem>
-      </NavBar>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/video/:id" component={VideoPage} />
-      </Switch>
+      <AuthProvider>
+        <FavoritesProvider>
+          <GlobalStyle />
+          <TopBar />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/video/:id" component={VideoPage} />
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/favorites" component={FavoritesPage} />
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </FavoritesProvider>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
-
-// import AuthProvider from '../../providers/Auth';
-// import HomePage from '../../pages/Home';
-// import LoginPage from '../../pages/Login';
-// import NotFound from '../../pages/NotFound';
-// import SecretPage from '../../pages/Secret';
-// import Private from '../Private';
-// import Fortune from '../Fortune';
-// import Layout from '../Layout';
-// import { random } from '../../utils/fns';
-
-// function App() {
-//   useLayoutEffect(() => {
-//     const { body } = document;
-
-//     function rotateBackground() {
-//       const xPercent = random(100);
-//       const yPercent = random(100);
-//       body.style.setProperty('--bg-position', `${xPercent}% ${yPercent}%`);
-//     }
-
-//     const intervalId = setInterval(rotateBackground, 3000);
-//     body.addEventListener('click', rotateBackground);
-
-//     return () => {
-//       clearInterval(intervalId);
-//       body.removeEventListener('click', rotateBackground);
-//     };
-//   }, []);
-
-//   return (
-//     <BrowserRouter>
-//       <AuthProvider>
-//         <Layout>
-//           <Switch>
-//             <Route exact path="/">
-//               <HomePage />
-//             </Route>
-//             <Route exact path="/login">
-//               <LoginPage />
-//             </Route>
-//             <Private exact path="/secret">
-//               <SecretPage />
-//             </Private>
-//             <Route path="*">
-//               <NotFound />
-//             </Route>
-//           </Switch>
-//           <Fortune />
-//         </Layout>
-//       </AuthProvider>
-//     </BrowserRouter>
-//   );
-// }
 
 export default App;
